be96469140ef5d69d113b37f563c20362f6ab6e1
stkorn <s.sommechai@gmail.com>
2019-02-02 19:21:59 +0700

    Create login activity

---
+++ app/src/main/java/com/ekstkorn/hospitalporterapp/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.ekstkorn.hospitalporterapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+}
